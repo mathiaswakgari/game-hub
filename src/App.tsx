b2f54afc64a0fce39201e38dccf167a34c2a49fb
Mathias Wakgari <mathiaswakgari12@gmail.com>
2023-07-27 15:19:26 +0300

    Responsive Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
 import "./App.css";
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="green">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `
+                "navbar" 
+                "main"`,
+        lg: `
+                "navbar navbar" 
+                "aside  main"`,
+      }}
+    >
+      <GridItem area={"navbar"} bg={"red"}>
+        Navbar
+      </GridItem>
+      <GridItem area={"main"} bg={"blue"}>
+        Main
+      </GridItem>
+      <Show above="lg">
+        <GridItem area={"aside"} bg={"green"}>
+          Aside
+        </GridItem>
+      </Show>
+    </Grid>
+  );
 }
 
 export default App;
