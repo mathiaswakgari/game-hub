d80d7d57d908af530509d66ff643a7201db92ef2
Mathias Wakgari <mathiaswakgari12@gmail.com>
2023-07-30 17:37:32 +0300

    Fetch Genres

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
+import GenresList from "./components/GenresList";
 
 function App() {
   return (
@@ -22,7 +23,9 @@ function App() {
         <GameGrid />
       </GridItem>
       <Show above="lg">
-        <GridItem area={"aside"}>Aside</GridItem>
+        <GridItem area={"aside"}>
+          <GenresList />
+        </GridItem>
       </Show>
     </Grid>
   );
---
+++ src/hooks/useGenres.ts
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import apiClinet from "../services/api-clinet";
+import { CanceledError } from "axios";
+
+export interface Genre {
+  id: number;
+  name: string;
+}
+
+interface FetchGenres {
+  count: number;
+  results: Array<Genre>;
+}
+
+const useGenres = () => {
+  const [genres, setGenres] = useState<Genre[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    setIsLoading(true);
+    apiClinet
+      .get<FetchGenres>("/genres", { signal: controller.signal })
+      .then((res) => {
+        setGenres(res.data.results);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        if (error instanceof CanceledError) return;
+        setError(error.message);
+        setIsLoading(false);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { genres, error, isLoading };
+};
+
+export default useGenres;
