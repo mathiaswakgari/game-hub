26fb737af0f0c7aeee588db2f5e03d31e6061966
Mathias Wakgari <mathiaswakgari12@gmail.com>
2023-07-31 15:21:52 +0300

    Fixed games with no images

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import placeHolder from "../assets/no-image-placeholder.webp";
+
 const getCroppedImage = (url: string) => {
-  if (!url) return "";
+  if (!url) return placeHolder;
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
