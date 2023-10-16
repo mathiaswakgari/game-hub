8140079953d08515dac21d6a215f7f0cf9eb3a6a
Mathias Wakgari <mathiaswakgari12@gmail.com>
2023-10-16 18:44:07 +0300

    Define a reusable api-client

--- src/services/api-clinet.ts
+++ src/services/api-clinet.ts
@@ -1,8 +1,19 @@
 import axios from "axios";
 
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
     key: "039fede072374603b3928b8ef9faaa83",
   },
 });
+
+class ApiClient<T> {
+  endpoint: string;
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = () => axiosInstance.get<T>(this.endpoint).then((res) => res.data);
+}
+
+export default ApiClient;
