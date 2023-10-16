f8632b4ee17885d46993bf084d39a6e15223c723
Mathias Wakgari <mathiaswakgari12@gmail.com>
2023-10-16 19:08:26 +0300

    initial data from file

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,12 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import genresService, { FetchGenres } from "../services/genresService";
-// import genres from "../data/Genres";
+import genres from "../data/Genres";
 
 const useGenres = () => {
   return useQuery<FetchGenres, Error, FetchGenres, string[]>({
     queryKey: ["genres"],
     queryFn: genresService.getAll,
     staleTime: 10 * 1000,
+    initialData: {
+      count: genres.length,
+      results: genres,
+    },
   });
 };
 
