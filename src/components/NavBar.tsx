5d40314f19d8457355e246a75d075500907d59ba
Mathias Wakgari <mathiaswakgari12@gmail.com>
2023-10-20 16:50:24 +0300

    Fixed searching.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,11 +2,15 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitchButton from "./ColorModeSwitchButton";
 import SearchBar from "./SearchBar";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <HStack padding={"0 10px"}>
-      <Image src={logo} boxSize={"100px"} />
+      <Link to="/">
+        <Image src={logo} boxSize={"60px"} objectFit={"cover"} />
+      </Link>
+
       <SearchBar />
       <ColorModeSwitchButton />
     </HStack>
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,16 +2,19 @@ import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 import useGameQueryStoreStore from "../stores/gameQueryStore";
+import { useNavigate } from "react-router-dom";
 
 const SearchBar = () => {
   const setSearchText = useGameQueryStoreStore((s) => s.setSearchText);
   const ref = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
 
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
         if (ref.current) setSearchText(ref.current?.value);
+        navigate("/");
       }}
     >
       <InputGroup>
