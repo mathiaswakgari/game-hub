61c25ec7d534de7aff7ce91dd5858a30d7bdfbff
Mathias Wakgari <mathiaswakgari12@gmail.com>
2023-10-20 10:32:31 +0300

    Handle errors

---
+++ src/pages/Error.tsx
@@ -0,0 +1,22 @@
+import { Box, Heading, Text, VStack } from "@chakra-ui/react";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+import NavBar from "../components/NavBar";
+
+const Error = () => {
+  const error = useRouteError();
+  return (
+    <>
+      <NavBar />
+      <VStack marginTop={5}>
+        <Heading>Oops</Heading>
+        <Text>
+          {isRouteErrorResponse(error)
+            ? "This page does not exit"
+            : "An unexpected error occurred"}
+        </Text>
+      </VStack>
+    </>
+  );
+};
+
+export default Error;
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,14 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
 import GameDetail from "./pages/GameDetail";
 import HomePage from "./pages/HomePage";
+import Error from "./pages/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <Error />,
+
     children: [
       { index: true, element: <HomePage /> },
       { path: "games/:id", element: <GameDetail /> },
